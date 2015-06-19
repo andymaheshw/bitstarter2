6152ba25394c1710104bd116bc07f28301cfcc16
Anirudh M <andymaheshw@gmail.com>
2015-06-19 01:17:32 +0000

    123

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express')
 var app = express();
-var fs.require('fs');
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  
+var fs = require('fs'); 
 var data = fs.readFileSync('index.html');
 response.send(data.toString('utf-8'))
 
